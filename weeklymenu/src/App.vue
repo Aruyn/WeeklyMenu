100a6213a61dd0b3d8c778ca0c7239bfe5d79d57
Sindre Nordbøe Årøen <sna@dips.no>
2022-04-21 15:37:30 +0200

    Added new logo for fun

--- weeklymenu/src/App.vue
+++ weeklymenu/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/menu.png">
+  <p>Her kommer ukes menyen</p>
 </template>
 
 <script lang="ts">
